230f788692ecb1ec79de6fdeddb43dff19229f3f
Ankit Bansal <bansalankit1575@gmail.com>
2026-01-08 10:49:26 +0530

    Initial Commit

---
+++ Expense-tracker/Expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_trackerApp.swift
+//  Expense-tracker
+//
+//  Created by Ankit bansal on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
